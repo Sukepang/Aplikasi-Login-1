1f2021563b3726b2d469721519007babc30651d9
Sukepang <135048964+Sukepang@users.noreply.github.com>
2024-02-25 00:19:24 +0700

    Update login-script.js
    mengganti komentar sesuai ddengan format jsdoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,22 +4,35 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
-
+/* Comment : membuat variable untuk setiap elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* membuat variabel untuk menyimpan informasi email dan password*/
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /* mendapatkan nilai dari masing-masing input(email dan password) ketika tombol ditekan*/
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+  
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
+
+    /* jika sesuai program akan berpindah ke halaman home */
+    if (email == expectedEmail && password == expectedPassword){
+      goToHome();
+
+    /* jika tidak sesuaai akan menampilkan pop up bahwa input salah */
+    } else {
+      showPopUp();
+    }
 });
